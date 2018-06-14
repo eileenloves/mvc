c6e8a76ec5a37a453d4fd30adf2d65d17d17d29e
Eileen Kim <eileenkim1208@gmail.com>
2018-06-14 16:06:59 +0900

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// This is test
 		System.out.println("Test");
-		System.out.println("Test");
+		System.out.println("est2");
 	}
 
 }
