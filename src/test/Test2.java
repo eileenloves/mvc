261962057c9b90e7a21d0daccd2864f79ccead52
Eileen <eileenkim1208@gmail.com>
2018-06-14 15:37:06 +0900

    change

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// This is test
 		System.out.println("Test");
-		System.out.println("Test");
+		System.out.println("test");
 	}
 
 }
